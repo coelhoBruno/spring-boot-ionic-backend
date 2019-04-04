54820ebab04ff76e87f4c436f9bbce34a0db3093
Bruno Coelho <coelho1_bruno@hotmail.com>
2019-04-04 13:00:31 -0300

    Deletando uma Categoria

--- src/main/java/com/estudo/cursomc/services/CategoriaService.java
+++ src/main/java/com/estudo/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.estudo.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.estudo.cursomc.domain.Categoria;
 import com.estudo.cursomc.repositories.CategoriaRepository;
+import com.estudo.cursomc.services.exceptions.DataIntegrityException;
 import com.estudo.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -31,5 +33,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch(DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não é possível excluir uma categoria que contem produtos vinculados.");
+		}
+		
 	}
 }
