82a88a8e8a15285897f234b224ca0a9e225f4fff
Bruno Coelho <coelho1_bruno@hotmail.com>
2019-04-15 13:12:47 -0300

    Listando Categorias All com DTO

--- src/main/java/com/estudo/cursomc/services/CategoriaService.java
+++ src/main/java/com/estudo/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.estudo.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,5 +44,9 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que contem produtos vinculados.");
 		}
 		
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
