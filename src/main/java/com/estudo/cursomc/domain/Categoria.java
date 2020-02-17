f94f1181994942a19c75d16bc03db823aa8fc937
Bruno Coelho da Silva <coelho1_bruno@hotmail.com>
2020-02-17 13:12:21 -0300

    Adiciona funcionalidade de Esqueci a senha

--- src/main/java/com/estudo/cursomc/domain/Categoria.java
+++ src/main/java/com/estudo/cursomc/domain/Categoria.java
@@ -10,7 +10,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
+@Getter @Setter
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
@@ -30,29 +34,29 @@ public class Categoria implements Serializable{
 		this.nome = nome;
 	}
 
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	public List<Produto> getProdutos() {
-		return produtos;
-	}
-
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
-	}
+//	public Integer getId() {
+//		return id;
+//	}
+//
+//	public void setId(Integer id) {
+//		this.id = id;
+//	}
+//
+//	public String getNome() {
+//		return nome;
+//	}
+//
+//	public void setNome(String nome) {
+//		this.nome = nome;
+//	}
+//	
+//	public List<Produto> getProdutos() {
+//		return produtos;
+//	}
+//
+//	public void setProdutos(List<Produto> produtos) {
+//		this.produtos = produtos;
+//	}
 
 	@Override
 	public int hashCode() {
